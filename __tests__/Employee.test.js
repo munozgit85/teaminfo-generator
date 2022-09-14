7765112fbd28f2836a0fa3017f61197e4f7eb29d
munozgit85 <107218022+munozgit85@users.noreply.github.com>
2022-09-14 12:33:51 -0500

    Engineer completed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,7 +3,7 @@ const Employee = require('../lib/Employee');
 
 //Test for Employee object 
 test(`create properties`, () => {
-    const employee = new Employee('Racquel', '37', 'racquelg85@hotmail.com');
+    const employee = new Employee('Racquel', 37, 'racquelg85@hotmail.com');
 
     expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toString(expect.any(Number));
@@ -13,7 +13,7 @@ test(`create properties`, () => {
 
 //Get employee name from id 
 test(`get employee name from id`, () => {
-    const employee = new Employee ('Racquel', '37', 'racquelg85@hotmail.com');
+    const employee = new Employee ('Racquel', 37, 'racquelg85@hotmail.com');
 
     expect(employee.getName()).toEqual(expect.any(String));
 
@@ -21,21 +21,21 @@ test(`get employee name from id`, () => {
 
 //Get id from employee id 
 test(`get employee ID number`, () => {
-    const employee = new Employee ('Racquel', '37', 'racquelg85@hotmail.com');
+    const employee = new Employee ('Racquel', 37, 'racquelg85@hotmail.com');
 
     expect(employee.getId()).toString(expect.any(Number));
 });
 
 //Get email from getEmail
 test('get employee email', () => { 
-    const employee = new Employee ('Racquel', '37', 'racquelg85@hotmail.com');
+    const employee = new Employee ('Racquel', 37, 'racquelg85@hotmail.com');
 
     expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
 });
 
 //Get role from getRole
 test('gets role of employee', () => {
-    const employee = new Employee ('Racquel', '37', 'racquelg85@hotmail.com');
+    const employee = new Employee ('Racquel', 37, 'racquelg85@hotmail.com');
 
     expect(employee.getRole()).toEqual("Employee");
 });
---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,23 @@
+// file for Engineer contructor function 
+const Engineer = require('../lib/Engineer');
+
+// Test for Engineer properties 
+test('creates an Engineer properties', () => {
+    const engineer = new Engineer('Racquel', 37, 'racquelg85@hotmail.com', 'munozgit85');
+    
+    expect(engineer.github) .toEqual(expect.any(String));
+});
+
+// getGithub() test function to obtain github username
+test('gets engineer github username', () => {
+    const engineer = new Engineer('Racquel', 37, 'racquelg85@hotmail.com', 'munozgit85');
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+// gets role from getRole() 
+test('assign role as Engineer', () => {
+    const engineer = new Engineer('Racquel', 37, 'racquelg85@hotmail.com', 'munozgit85');
+
+    expect(engineer.getRole()).toEqual("Engineer");
+});
